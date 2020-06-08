c59d301bdddd1210d522fb5b9d6f207f79b0b8cd
Paul <pdeyarmin@gmail.com>
2020-06-08 08:50:26 -0400

    overview page is mostly styling but needs to pull from existing cases, moving to create case repository

--- case-tracker/src/main/java/org/launchcode/casetracker/controllers/HomeController.java
+++ case-tracker/src/main/java/org/launchcode/casetracker/controllers/HomeController.java
@@ -1,12 +1,17 @@
 package org.launchcode.casetracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping
-    public String index() { return "index"; }
+    @RequestMapping("")
+    public String index(Model model) {
+        model.addAttribute("title", "Overview");
+        return "index";
+    }
 
 }
